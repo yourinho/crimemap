4849a373acfea96f9fc20c03711d26b59be796ba
yuragasyuk <yura.gasyuk@gmail.com>
2017-12-05 20:21:37 +0300

    Add mitigating against SQL injection

--- dbhelper.py
+++ dbhelper.py
@@ -25,9 +25,9 @@ class DBHelper:
         connection = self.connect()
         try:
             # The following introduces a deliberate security flaw (SQL injection).
-            query = "INSERT INTO crimes (description) VALUES ('{}');".format(data)
+            query = "INSERT INTO crimes (description) VALUES (%s);"
             with connection.cursor() as cursor:
-                cursor.execute(query)
+                cursor.execute(query, data)
                 connection.commit()
         finally:
             connection.close()
